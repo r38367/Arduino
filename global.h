954ed266fbd405845fb949b6b81886ed41f16cda
r38367 <r38367@gmail.com>
2020-12-15 19:55:57 +0100

    Create global.h

---
+++ global.h
@@ -0,0 +1,6 @@
+#include "global.h"
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
