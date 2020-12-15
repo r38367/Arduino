5c00ba4b9c9d43b873df8e3d9d5aba230d5218e3
r38367 <r38367@gmail.com>
2020-12-15 19:59:43 +0100

    Add #ifndef

--- layout.h
+++ layout.h
@@ -2,7 +2,8 @@
  * Board pin layout
  */
 // constants won't change. They're used here to set pin numbers:
-
+#ifndef LAYOUT_H
+#define LAYOUT_H
 const int LCD_RS  = 2; 
 const int LCD_EN  = 3; 
 const int LCD_D4  = 4; 
@@ -15,3 +16,4 @@ const int SIMULATOR_PIN = 11; // HIGH if simulation
 const int BUTTON_PIN  = 13;  // the number of the pushbutton pin
 
 const int SENSOR_PIN = A5; // int Sensor = Analog 5.
+#endif
