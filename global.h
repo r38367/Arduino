41e6781775fcdbb7f88c5116282027badc61acc0
r38367 <r38367@gmail.com>
2020-12-15 20:00:55 +0100

    Add #ifnedf

--- global.h
+++ global.h
@@ -1,6 +1,13 @@
-#include "global.h"
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "layout.h"
 #include <LiquidCrystal.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+
+
+#endif
