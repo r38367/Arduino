d30a4256aee4a1e3d1906f9a7d30d6687a471688
r38367 <r38367@gmail.com>
2020-12-15 19:50:07 +0100

    Create layout.h

---
+++ layout.h
@@ -0,0 +1,17 @@
+/*
+ * Board pin layout
+ */
+// constants won't change. They're used here to set pin numbers:
+
+const int LCD_RS  = 2; 
+const int LCD_EN  = 3; 
+const int LCD_D4  = 4; 
+const int LCD_D5  = 5; 
+const int LCD_D6  = 6; 
+const int LCD_D7  = 7;
+
+const int MOTOR_PIN = 8;   // LED lit when motor is on
+const int SIMULATOR_PIN = 11; // HIGH if simulation
+const int BUTTON_PIN  = 13;  // the number of the pushbutton pin
+
+const int SENSOR_PIN = A5; // int Sensor = Analog 5.
